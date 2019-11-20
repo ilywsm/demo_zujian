d84955ca5d9a5a17c24a551ab40083a7cdd80e77
xu <962741212@qq.com>
2019-11-20 09:56:40 +0800

    配置中心

---
+++ src/main/java/com/liuxu/demo_zujian/DemoZujianApplication.java
@@ -0,0 +1,13 @@
+package com.liuxu.demo_zujian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZujianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoZujianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liuxu/demo_zujian/DemoZujianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuxu.demo_zujian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoZujianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
