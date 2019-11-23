8a761bc9384c360bceb2bc774d27d00145653f67
xu <962741212@qq.com>
2019-11-23 08:46:40 +0800

    全部

--- demo_provider/src/main/java/com/liuxu/controller/UserController.java
+++ demo_provider/src/main/java/com/liuxu/controller/UserController.java
@@ -4,6 +4,10 @@ import com.liuxu.entity.Classs;
 import com.liuxu.entity.User;
 import com.liuxu.service.UserService;
 import com.liuxu.vo.UserVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,12 +16,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+@Api(tags = "关于学生表得增删改查")
 @RestController
 @CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
+    @ApiOperation(value = "关于学生表得查询分页操作")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query",name = "pageNo",value = "分页",required = true,dataType = "Integer"),
+/*
+            @ApiImplicitParam(paramType = "query",name = "userVo",value = "查询",required = true,dataType = "UserVo")
+*/
+    })
     @RequestMapping("/list")
     public Page<User> list(@RequestParam(defaultValue = "1")Integer pageNo, UserVo userVo){
         return userService.list(PageRequest.of(pageNo-1,3),userVo);
